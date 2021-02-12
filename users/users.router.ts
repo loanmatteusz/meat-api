a4d5442ccef1b06d82fdd4f42074e201ee1c324c
Loan Matteus <loan.matteusz@gmail.com>
2021-02-12 18:06:15 -0300

    chore: create authentication

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,6 +1,7 @@
 import * as restify from 'restify';
 
 import { ModelRouter } from '../common/model-router';
+import { authenticate } from '../security/auth.handler';
 import { User } from './users.model';
 
 class UsersRouter extends ModelRouter<User> {
@@ -35,6 +36,8 @@ class UsersRouter extends ModelRouter<User> {
     application.put(`${this.basePath}/:id`, this.validateId, this.replace);
     application.patch(`${this.basePath}/:id`, this.validateId, this.update);
     application.del(`${this.basePath}/:id`, this.validateId, this.delete);
+
+    application.post(`${this.basePath}/authenticate`, authenticate);
   }
 }
 
